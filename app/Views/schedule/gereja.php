d472b5fd25ba52832b592c0a5b042c910127b4fe
Djordi <djordiymarbun@gmail.com>
2022-11-12 08:45:00 +0700

    Perbaikan header

--- app/Views/schedule/gereja.php
+++ app/Views/schedule/gereja.php
@@ -1,6 +1,47 @@
+<!-- <div class="container">
+   <h3>Schedule Gereja</h3>
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis minima officia accusantium,
+      vero quisquam optio! Totam unde sit nobis qui. Dignissimos placeat dolorem blanditiis numquam?
+      Iusto officiis fugiat aspernatur rerum.</p>
+</div> -->
+<p align="center">Ini bisa di ganti2 di panel admin</p>
 <div class="container">
-  <h3>Schedule Gereja</h3>
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis minima officia accusantium, 
-   vero quisquam optio! Totam unde sit nobis qui. Dignissimos placeat dolorem blanditiis numquam? 
-   Iusto officiis fugiat aspernatur rerum.</p>
+   <h2 align="center">Jadwal Ibadah Gereja </h2>
+   <br><br>
+   <div class="col-md-6">
+      <div class="panel panel-primary">
+         <div class="panel-heading">
+            <h4 align="center">Kebaktian Umum</h4>
+         </div>
+         <div class="panel-body">Setiap Minggu pk. 08.00 WIB</div>
+      </div>
+   </div>
+
+   <div class="col-md-6">
+      <div class="panel panel-success">
+         <div class="panel-heading">
+            <h4 align="center">Kebaktian Anak</h4>
+         </div>
+         <div class="panel-body">Setiap Minggu pk. 08.00 WIB</div>
+      </div>
+   </div>
+
+   <div class="col-md-6">
+      <div class="panel panel-info">
+         <div class="panel-heading">
+            <h4 align="center">Kebaktian Remaja</h4>
+         </div>
+         <div class="panel-body">Setiap Minggu pk. 08.00 WIB</div>
+      </div>
+   </div>
+
+   <div class="col-md-6">
+      <div class="panel panel-warning">
+         <div class="panel-heading">
+            <h4 align="center">Kebaktian Pemuda </h4>
+         </div>
+         <div class="panel-body">Setiap Minggu pk. 08.00 WIB</div>
+      </div>
+   </div>
+
 </div>
--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,39 +9,38 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="<?php echo base_url()?>/assets/css/style.css">
+	<link rel="stylesheet" href="<?php echo base_url() ?>/assets/css/style.css">
 </head>
+
 <body>
 
-<nav class="navbar navbar-default">
-  <div class="container">
-	 <div class="navbar-header">
-		<a class="navbar-brand" href="<?php echo base_url()?>">GPM LATERI</a>
-	 </div>
-	 <ul class="nav navbar-nav navbar-right">
-		<li><a href="<?php echo base_url()?>/about" id="about">Tentang Kami</a></li> <!-- class="active" -->
-		<li><a href="<?php echo base_url()?>/renungan" id="renungan">Renungan</a></li>
-		<li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="news">Berita & Peristiwa
-        <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="<?php echo base_url()?>/news/gereja">Gereja</a></li>
-          <li><a href="<?php echo base_url()?>/news/sektor">Sektor</a></li>
-        </ul>
-      </li>
-		<li><a href="<?php echo base_url()?>/jemaat" id="jemaat">Informasi Jemaat</a></li>
-		<li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="schedule">Jadwal Ibadah
-        <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="<?php echo base_url()?>/schedule/gereja">Gereja</a></li>
-          <li><a href="<?php echo base_url()?>/schedule/sektor">Sektor</a></li>
-          <li><a href="<?php echo base_url()?>/schedule/unit">Unit</a></li>
-        </ul>
-      </li>
-		<li><a href="#" id="contact">Contact Us</a></li>
-	 </ul>
-  </div>
-</nav>
-
-	
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="<?php echo base_url() ?>">GEREJA KEHIDUPAN</a>
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="<?php echo base_url() ?>/about" id="about">Tentang Kami</a></li> <!-- class="active" -->
+				<li><a href="<?php echo base_url() ?>/renungan" id="renungan">Renungan</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="news">Berita & Peristiwa
+						<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="<?php echo base_url() ?>/news/gereja">Gereja</a></li>
+						<li><a href="<?php echo base_url() ?>/news/sektor">Sektor</a></li>
+					</ul>
+				</li>
+				<li><a href="<?php echo base_url() ?>/jemaat" id="jemaat">Informasi Jemaat</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="schedule">Jadwal Ibadah
+						<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="<?php echo base_url() ?>/schedule/gereja">Gereja</a></li>
+						<li><a href="<?php echo base_url() ?>/schedule/sektor">Sektor</a></li>
+						<li><a href="<?php echo base_url() ?>/schedule/unit">Unit</a></li>
+					</ul>
+				</li>
+				<li><a href="#" id="contact">Contact Us</a></li>
+			</ul>
+		</div>
+	</nav>
